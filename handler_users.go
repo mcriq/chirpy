976ddac13beb89e63a0c2d0fa56dacd93f68bf3a
mcriq <125591030+mcriq@users.noreply.github.com>
2025-02-22 13:07:52 -0600

    add users functionality

---
+++ handler_users.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+	type email struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	emailParams := email{}
+	err := decoder.Decode(&emailParams)
+	if err != nil {
+		log.Printf("Error decoding user parameters: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	user, err := cfg.dbQueries.CreateUser(r.Context(), emailParams.Email)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			log.Printf("unable to create user: %v", err)
+			w.WriteHeader(http.StatusConflict)
+			return
+		}
+		log.Printf("unable to create user: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	userParams := User{
+		ID: user.ID, 
+		CreatedAt: user.CreatedAt, 
+		UpdatedAt: user.UpdatedAt, 
+		Email: user.Email,
+	}
+	
+	err = writeJSON(w, http.StatusCreated, userParams)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	err := cfg.dbQueries.DeleteUsers(r.Context())
+	if err != nil {
+		fmt.Printf("unable to delete users: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mcriq/chirpy/internal/database"
 )
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
+	platform string
 }
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("unable to open database")
+	}
+
+	dbQueries := database.New(db)
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries: dbQueries,
+		platform: platform,
 	}
 
 	mux := http.NewServeMux()
@@ -24,6 +44,8 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsers)
+	// mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirp)
 	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
@@ -31,6 +53,5 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
-
 }
 
