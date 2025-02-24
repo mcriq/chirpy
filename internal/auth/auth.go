3df446f260b37aca7366c07a3f53113e6d434098
mcriq <125591030+mcriq@users.noreply.github.com>
2025-02-24 16:10:52 -0600

    add internal auth code for jwt tokens

---
+++ internal/auth/auth.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	pw := []byte(password)
+	dat, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("error generating pw hash")
+		return "", err
+	}
+	return string(dat), nil
+}
+
+func CheckPasswordHash(password string, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	regClaims := jwt.RegisteredClaims{
+		Issuer: "chirpy", 
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()), 
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)), 
+		Subject: userID.String(),
+	}
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, regClaims)
+	tokenStr, err := newToken.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
+		userID, err := uuid.Parse(claims.Subject)
+		if err != nil {
+			return uuid.Nil, err
+		}
+		return userID, nil
+	}
+	
+	return uuid.Nil, err
+}
+
+func GetBearerToken(headers http.Header) (string, error){
+	
+}
