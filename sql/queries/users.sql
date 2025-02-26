322e45e822f25785d4bc1386927ceb685d7faf01
mcriq <125591030+mcriq@users.noreply.github.com>
2025-02-26 07:25:54 -0600

    create query for upgrading membership to chirpy red by id

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -29,3 +29,8 @@ UPDATE users
 SET email = $1, hashed_password = $2, updated_at = NOW()
 WHERE id = $3
 RETURNING id, email, created_at, updated_at;
+
+-- name: UpgradeToChirpyRedById :exec
+UPDATE users
+SET is_chirpy_red = true, updated_at = NOW()
+WHERE id = $1;
